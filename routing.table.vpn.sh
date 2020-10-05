4cb06c4623a128f6f3895cfd2f7cf20648f7b924
Carlos Vigo <carlos@carlos-vigo.com>
2020-10-05 09:26:18 +0200

    More domains and more private subnets

--- routing.table.vpn.sh
+++ routing.table.vpn.sh
@@ -7,14 +7,14 @@ fi
 
 #These might be different in your system
 WIRELESS_INTERFACE=en0 #WiFi
-# WIRELESS_INTERFACE=en8 #modem 4g
+#WIRELESS_INTERFACE=en8 #modem 4g
 TUNNEL_INTERFACE=ppp0
 
 #this might need to be changed in Linux
 GATEWAY=$(netstat -nrf inet | grep default | grep $WIRELESS_INTERFACE | awk '{print $2}')
 
 # Request to IPs within the following network masks are routed through the VPN interface. Add as many as you need.
-DEST_NETS="192.168/16 23.4/16 22.17/16 10.48/16 10.51/16 10.50/16 10.111/16"
+DEST_NETS="192.168/16 23.4/16 22.17/16 10.48/16 10.51/16 10.50/16 10.111/16 100.80/16 100.71/16 34.246/16 34.243/16"
 
 # Default router is set to use the WiFi interface
 echo "Resetting routes with gateway => $GATEWAY"
@@ -35,7 +35,7 @@ do
 done
 
 #setting up the name resolvers for well known domain suffix
-for suffix in es com net org be co au edu biz io tv eu nz uk ie bbva gl gal nl ee in help de fm it sh
+for suffix in es com net org be co au edu biz io tv eu nz uk ie bbva gl gal nl ee in help de fm it sh ly gov do info to be dev fr la google me online gle link app ca cc gd gq cz tech
 do
     echo 'nameserver 8.8.8.8' | tee /etc/resolver/$suffix
 done
