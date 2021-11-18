5883781a5674998fa18d7c95750eb58fc9188d8d
Carlos Vigo <carlos@carlos-vigo.com>
2021-11-18 09:19:33 +0100

    more nets

--- routing.table.vpn.sh
+++ routing.table.vpn.sh
@@ -14,7 +14,8 @@ TUNNEL_INTERFACE=ppp0
 GATEWAY=$(netstat -nrf inet | grep default | grep $WIRELESS_INTERFACE | awk '{print $2}')
 
 # Request to IPs within the following network masks are routed through the VPN interface. Add as many as you need.
-DEST_NETS="192.168/16 23.4/16 22.17/16 10.48/16 10.51/16 10.50/16 10.111/16 100.80/16 100.71/16 34.246/16 34.243/16 10.52/16 10.55/16"
+# DEST_NETS="192.168/16 23.4/16 22.17/16 10.48/16 10.51/16 10.50/16 10.111/16 100.80/16 100.71/16 34.246/16 34.243/16 10.52/16 10.55/16 150.250/16"
+DEST_NETS="23.4/16 22.17/16 10.48/16 10.51/16 10.50/16 10.111/16 100.80/16 100.71/16 34.246/16 34.243/16 10.52/16 10.55/16 150.250/16"
 
 # Default router is set to use the WiFi interface
 echo "Resetting routes with gateway => $GATEWAY"
@@ -35,7 +36,7 @@ do
 done
 
 #setting up the name resolvers for well known domain suffix
-for suffix in es com net org be co au edu biz io tv eu nz uk ie bbva gl gal nl ee in help de fm it sh ly gov do info to be dev fr la google me online gle link app ca cc gd gq cz tech build
+for suffix in es com net org be co au edu biz io tv eu nz uk ie bbva gl gal nl ee in help de fm it sh ly gov do info to be dev fr la google me online gle link app ca cc gd gq cz tech build cafe studio us help
 do
     echo 'nameserver 8.8.8.8' | tee /etc/resolver/$suffix
 done
